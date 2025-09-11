28c7fa00819fdb444de748681e205b12ef271ab1
Dia8897 <khalildia831@gmail.com>
2025-09-12 00:44:09 +0300

    layout

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+//    	
 
         
 //         BorderPane root = createBorderPaneSkeleton();       // Step 1
